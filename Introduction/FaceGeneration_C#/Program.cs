d899bf0caf54c982f4487240c16cd8e7948e635b
hamidrezazarrabi <steven.zarrabi@gmail.com>
2024-10-14 10:43:07 +0330

    UseAPI

---
+++ Introduction/FaceGeneration_C#/Program.cs
@@ -0,0 +1,8 @@
+﻿var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, "https://thispersondoesnotexist.com");
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+var imgStream = await response.Content.ReadAsStreamAsync();
+
+ using var fileStream = new FileStream("FakeImage.jpg", FileMode.Create);
+ await imgStream.CopyToAsync(fileStream);
---
+++ Introduction/README.md
@@ -0,0 +1,3 @@
+# Using API
+We are using to test some available APIs and use them. File `UseAPI.ipynb` contains four APIs written in Python and Folder `FaceGeneration_C#` contains one API written in C#.
+
